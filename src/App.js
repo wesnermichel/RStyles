bd9791e1f31f871ce94474499fabd9a5cbab2fb9
Wesner M <wesnerm@gmail.com>
2023-07-13 15:55:00 -0400

    stitchingcss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import "./App.css";
+// import "./App.css";
+//  import styles from "./App.module.css";
+// import "./App.scss"
+// import styled from "styled-components"
+import { styled } from '@stitches/react';
 
+// const FancyHeading= styled.h1`
+// background-color: red;
+// border: 3px solid purple:
+// `
 
+// const FancyHeading = styled("h1", {
+//   backgroundColor: "red",
+//   border: "3px solid orange"
+// })
 function App() {
 
   return (
   <div className="App">
-    <h1 style= {{}}>Hello World</h1>
+    {/* <FancyHeading>Hello World</FancyHeading> */}
     </div>
   )
 }

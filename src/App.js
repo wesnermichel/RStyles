f1bb4e6de6f02fa67427017a2574dacb0f0c5794
Wesner M <wesnerm@gmail.com>
2023-07-13 19:34:14 -0400

    JSSintro

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 //  import styles from "./App.module.css";
 // import "./App.scss"
 // import styled from "styled-components"
-import { styled } from '@stitches/react';
+// import { styled } from '@stitches/react';
 
 // const FancyHeading= styled.h1`
 // background-color: red;
@@ -13,11 +13,17 @@ import { styled } from '@stitches/react';
 //   backgroundColor: "red",
 //   border: "3px solid orange"
 // })
+
+import {css} from "@emotion/css"
 function App() {
+
+  const h1Class= css `
+  background-color:red;
+  `
 
   return (
   <div className="App">
-    {/* <FancyHeading>Hello World</FancyHeading> */}
+<h1 className={h1Class}>Hello World</h1>
     </div>
   )
 }
